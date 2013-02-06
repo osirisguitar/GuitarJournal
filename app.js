dcb9da2c83dcf447f53f7c8b57545aec80fe10c0
Anders Bornholm <anders@bornholm.se>
2013-02-06 21:05:31 +0100

    Move to root

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+/*app.get("/", function(req, res) {
+	res.send("Yippie!");
+});*/
+
+app.use('/app', express.static(__dirname + '/app'));
+
+app.listen(1337);
