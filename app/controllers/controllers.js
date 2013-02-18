c100d9f98b38338f4058c9db441ff90ddda70891
Anders Bornholm <anders@bornholm.se>
2013-02-18 23:55:39 +0100

    Added buttons for new session and edit session.

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -11,18 +11,28 @@ function HomeCtrl($scope) {
 	$scope.pageSettings.pageTitle = "Osiris Guitar Journal";
 	$scope.pageSettings.active = "home";
 	$scope.pageSettings.showBackButton = false;
+	$scope.pageSettings.rightButtonText = null;
 }
 
 function SessionsCtrl($scope, $http) {
 	$scope.pageSettings.pageTitle = "Sessions";
 	$scope.pageSettings.active = "sessions";
 	$scope.pageSettings.showBackButton = false;
+	$scope.pageSettings.rightButtonText = "New";
+	$scope.pageSettings.rightButtonClick = function() {
+		alert("New!")
+	};
 }
 
 function SessionCtrl($scope, $routeParams, $http) {
 	$scope.sessionId = $routeParams.id;
 	$scope.pageSettings.active = "sessions";
 	$scope.pageSettings.showBackButton = true;
+	$scope.pageSettings.rightButtonText = "Edit";
+
+	$scope.pageSettings.rightButtonClick = function() {
+		alert("Edit!")
+	};
 
 	// dummy code
 	for (i = 0; i < $scope.sessions.length; i++)  {
