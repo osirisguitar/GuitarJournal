55089318204fc24ef2bc7cb29d59be39341a11a7
Anders Bornholm <anders@bornholm.se>
2013-02-19 23:53:17 +0100

    Added Mongo DB code

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,10 +1,12 @@
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
 	$scope.pageSettings = {};
-	$scope.sessions = [
-		{ id: "0", date: "2013-01-01", "length": "15", "goal": "Lorem ipsum", "notes": "Bacon ipsum", "goal": "Spela fortare", "bpm": "130", "instrument": "Schecter Omen 7", "grade": "3" },
-		{ id: "1", date: "2013-01-03", "length": "47", "goal": "Lorem ipsum", "notes": "Bacon ipsum", "goal": "Spela fortare", "bpm": "140", "instrument": "Ibanez S470", "grade": "4" },
-		{ id: "2", date: "2013-01-04", "length": "31", "goal": "Lorem ipsum", "notes": "Bacon ipsum", "goal": "Spela fortare", "bpm": "120", "instrument": "Cort M520", "grade": "3" }
-	];
+	$http.get('/api/users/123/sessions')
+		.success(function(data) {
+			$scope.sessions = data;
+		})
+		.error(function(data) {
+			alert("Error when getting sessions.")
+		});
 }
 
 function HomeCtrl($scope) {
