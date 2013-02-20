cd5730266d44c344c157765a874a5eb2cd15f0ac
Anders Bornholm <anders@bornholm.se>
2013-02-20 22:05:23 +0100

    Added mongoose. Fixed new button for sessions.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var mongoose = require('mongoose');
+var mongoConnectionString = "mongodb://osiris:testmongo123@linus.mongohq.com:10003/app11622295";
 
-
+app.use(express.bodyParser());
 // App stuff, static files
 app.get("/", function(req, res) {
 	res.sendfile(__dirname + "/app/home.html");
@@ -18,8 +21,8 @@ app.use('/app', express.static(__dirname + '/app'));
 // API stuff
 app.get("/api/sessions", function(req, res) {
 
-// Connect to the db
-	MongoClient.connect("mongodb://osiris:testmongo123@linus.mongohq.com:10003/app11622295", function(err, db) {
+	// Connect to the db
+	MongoClient.connect(mongoConnectionString, function(err, db) {
 		if(err) { return console.dir(err); }
 
 		var collection = db.collection('Sessions');
@@ -27,7 +30,37 @@ app.get("/api/sessions", function(req, res) {
 			res.json(items);
 		});
 	});
-})
+});
+
+app.get("/api/session/:id", function(req, res) {
+
+	// Connect to the db
+	MongoClient.connect(mongoConnectionString, function(err, db) {
+		if(err) { return console.dir(err); }
+
+		var collection = db.collection('Sessions');
+		collection.findOne({ _id: ObjectID(req.params.id) }, function(err, item) {
+			res.json(item);
+		});
+	});
+});
+
+app.post("/api/sessions", function(req, res) {
+	console.log("Body:");
+	console.log(req.body);
+	if (req.body._id)
+	{
+		req.body._id = ObjectID(req.body._id);
+	}
+	MongoClient.connect(mongoConnectionString, function(err, db) {
+		if(err) { return console.dir(err); }
+
+		var collection = db.collection('Sessions');
+		collection.save(req.body, {safe:true}, function(err, savedSession) {
+			res.json(savedSession);
+		});
+	});
+});
 
 
 var port = process.env.PORT || 1337;
--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -16,23 +16,38 @@ function HomeCtrl($scope) {
 	$scope.pageSettings.rightButtonText = null;
 }
 
-function SessionsCtrl($scope, $http) {
+function SessionsCtrl($scope, $http, $location) {
 	$scope.pageSettings.pageTitle = "Sessions";
 	$scope.pageSettings.active = "sessions";
 	$scope.pageSettings.showBackButton = false;
 	$scope.pageSettings.rightButtonText = "New";
 	$scope.pageSettings.rightButtonClick = function() {
-		alert("New!")
+		$location.path('/session/');
 	};
 }
 
 function SessionCtrl($scope, $routeParams, $http) {
 	$scope.pageSettings.pageTitle = "Session";
-	$scope.sessionId = $routeParams.id;
 	$scope.pageSettings.active = "sessions";
 	$scope.pageSettings.showBackButton = true;
 	$scope.pageSettings.rightButtonText = "Edit";
 	$scope.editMode = false;
+
+	if ($routeParams.id != null && $routeParams.id != "")
+	{
+		$http.get('/api/session/' + $routeParams.id)
+			.success(function(data) {
+				$scope.session = data;
+			})
+			.error(function(data) {
+				alert("Error getting session: " + data);
+			});
+	}
+	else
+	{
+		$scope.session = { date: new Date() };
+		$scope.editMode = true;
+	}
 
 	$scope.pageSettings.rightButtonClick = function() {
 		$scope.editMode = true;
@@ -41,15 +56,13 @@ function SessionCtrl($scope, $routeParams, $http) {
 
 	$scope.save = function()
 	{
-		$scope.editMode = false;
-		$scope.pageSettings.showBackButton = true;
+		$http.post('/api/sessions', $scope.session)
+			.success(function(data) {
+				$scope.editMode = false;
+				$scope.pageSettings.showBackButton = true;
+				$scope.session = data;
+			})
+			.error(function(data) { alert("Error saving session: " + data)});
 	}
 
-	// dummy code
-	for (i = 0; i < $scope.sessions.length; i++)  {
-		if ($scope.sessions[i]._id == $scope.sessionId) {
-			$scope.session = $scope.sessions[i];
-			break;
-		}
-	}
 }
